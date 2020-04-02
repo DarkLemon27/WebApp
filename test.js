19a885fd450a265e6a0c59f06bdd1f67404a2e54
DarkLemon27 <47903566+DarkLemon27@users.noreply.github.com>
2020-04-02 15:09:59 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-<iframe id="hiderr" src="http://csec380-core.csec.rit.edu:86/friends.php"></iframe>
-<script>
-    var eff = document.getElementById("hiderr");
+function doIt() {
+    var eff = document.getElementById("hiddenone");
     eff.addEventListener("load",function(){
        var conter= eff.contentWindow.document.body.innerHTML.match('^[^<br]*');
        var cant= conter.split(',');
        alert(cant[0]);
        alert(cant[1]);
    });
-</script>
