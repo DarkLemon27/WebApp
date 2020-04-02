b531a04a13098a1ff97b1f8647799ee2d7c0c4ae
DarkLemon27 <47903566+DarkLemon27@users.noreply.github.com>
2020-04-02 15:31:38 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,9 +2,11 @@ function moveitmoveit() {
     var eff = document.getElementById("hiddenone");
     eff.addEventListener("load",function(){
        var conter= eff.contentWindow.document.body.innerHTML.match('^[^<br]*');
-       alert(conter);
-       alert(conter[0]);
-       alert(conter[1]);
+       var stringy = conter.join();
+       alert(stringy);
+       alert("[0] " + stringy[0]);
+       var arr = stringy.split(',');
+       alert(arr[0]);
    });
 }
 moveitmoveit();
