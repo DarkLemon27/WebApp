c5d9e2d0725d0c4c934ca13b2222c6f35aff29ac
DarkLemon27 <47903566+DarkLemon27@users.noreply.github.com>
2020-04-02 15:36:20 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,10 +3,13 @@ function moveitmoveit() {
     eff.addEventListener("load",function(){
        var conter= eff.contentWindow.document.body.innerHTML.match('^[^<br]*');
        var stringy = conter.join();
-       alert(stringy);
-       alert("[0] " + stringy[0]);
+       //alert(stringy);
+
        var arr = stringy.split(',');
-       alert(arr[0]);
+       for( var i = 0; i < arr.length; i++){
+           alert(arr[i]);
+       }
+       //alert(arr[0]);
    });
 }
 moveitmoveit();
