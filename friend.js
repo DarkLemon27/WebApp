4611eafc59f8482918abd7514003848431098871
DarkLemon27 <47903566+DarkLemon27@users.noreply.github.com>
2020-04-01 21:17:13 -0400

    added alerts

--- friend.js
+++ friend.js
@@ -1,9 +1,11 @@
 <iframe id="hid" src="http://csec380-core.csec.rit.edu:86/friends.php"></iframe>
 <script>
+    alert(1);
     var e = document.getElementById("hid");
     e.addEventListener("load",function(){
        var cont= e.contentWindow.document.body.innerHTML.match('^[^<br]*');
        alert(cont);
+       alert(2);
        //var numbers = e.matchAll('[^,]*');    # [22,4,5,2,1]
        //for( var num in numbers ){
        //    var string1 = "https://csec380-core.csec.rit.edu:86/add_friend.php?id=101";
