a64805ebd4474ee2840a3620f5c2e6ce6f22e4d0
DarkLemon27 <47903566+DarkLemon27@users.noreply.github.com>
2020-04-03 16:48:21 -0400

    Update friend.js

--- friend.js
+++ friend.js
@@ -9,12 +9,10 @@ function moveitmoveit() {
     });
     var eff = friend.responseText;
     alert(eff);
-    var conter= eff.match('^[^<br]*');
-    alert(2);
-    var stringy = conter.join();
-    alert(3);
-    var arr = stringy.split(',');
-    alert(4);
+    //var conter= eff.match('^[^<br]*');
+    //var stringy = conter.join();
+    //var arr = stringy.split(',');
+    var arr= eff.match('^[^<br]*').join().split(',');
     for( var i = 0; i < arr.length; i++){
         //  $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",{id: 131,comment: 'Boi'});
         alert(arr[i]);
