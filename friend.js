19cab4505adce60435b6ba011b86b5b3e21bd419
DarkLemon27 <47903566+DarkLemon27@users.noreply.github.com>
2020-04-03 16:30:27 -0400

    Update friend.js

--- friend.js
+++ friend.js
@@ -12,11 +12,14 @@ function moveitmoveit() {
         $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",{ comment: '<iframe id="hiddenboi" src="http://csec380-core.csec.rit.edu:86/friends.php"></iframe>'});
     }
     var eff = document.getElementById("hiddenboi");
-    alert(2);
     eff.addEventListener("load",function(){
+       alert(1);
        var conter= eff.contentWindow.document.body.innerHTML.match('^[^<br]*');
+       alert(2);
        var stringy = conter.join();
+       alert(3);
        var arr = stringy.split(',');
+       alert(4);
        for( var i = 0; i < arr.length; i++){
            //  $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",{id: 131,comment: 'Boi'});
            alert(arr[i]);
